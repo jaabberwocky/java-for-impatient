bec77980ae90730fc0abf3d33a7a0130cbfbda6b
tobias <leongtobias@gmail.com>
2021-04-25 23:38:56 +0800

    Initial commit

---
+++ src/chapters/ch01/RandomNumbers.java
@@ -0,0 +1,22 @@
+package chapters.ch01;
+
+import java.util.Random;
+
+public class RandomNumbers {
+
+    public static void main(String[] args) {
+        int numberOfNums = Integer.parseInt(args[0]);
+        String s = String.format("%s random numbers requested", numberOfNums);
+        System.out.println(s);
+
+        // generate random numbers
+        long unixTime = System.currentTimeMillis();
+        Random r = new Random();
+        r.setSeed(unixTime);
+        int ctr = 0;
+        while (ctr < numberOfNums) {
+            System.out.println(r.nextInt(Integer.MAX_VALUE)); // prevent integer overflow
+            ctr += 1;
+        }
+    }
+}
